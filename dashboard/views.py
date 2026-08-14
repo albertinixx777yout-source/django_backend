1b671bd325794f8b7e8a6d2c1343bb66bd6971f2
Albert <albertinixx777yout@gmail.com>
2026-08-13 20:53:48 -0500

    fix: configurar vistas y urls de la app dashboard

---
+++ dashboard/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import requests
+from django.conf import settings
+
+def index(request):
+    try:
+        response = requests.get(settings.API_URL)
+        posts = response.json()
+        total_responses = len(posts)
+    except Exception:
+        total_responses = 0
+
+    data = {
+        "title": "Landing Page Dashboard",
+        "total_responses": total_responses,
+    }
+    return render(request, "dashboard/index.html", data)
