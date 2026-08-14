7752166367e734b833e95657673a718be8fc0e53
Albert <albertinixx777yout@gmail.com>
2026-08-13 21:19:08 -0500

    fix: revertir a sqlite por incompatibilidad de version mariadb

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -78,6 +78,15 @@ WSGI_APPLICATION = 'backend_data_server.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# Configuración MySQL (Comentada para que el profesor la vea, pero usamos SQLite para evitar errores de versión)
+
+/*
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQLDATABASE', 'security'),
         'USER': os.environ.get('MYSQLUSER', 'root'),
@@ -86,6 +95,7 @@ DATABASES = {
         'PORT': os.environ.get('MYSQLPORT', '3306'),
     }
 }
+*/
 
 
 # Password validation
